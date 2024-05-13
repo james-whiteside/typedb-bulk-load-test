1e82e8515b245d860ae7367bb397935f20464d42
James Whiteside <james.whiteside@vaticle.com>
2024-05-13 21:24:37 +0100

    Added class for test batches.

--- bulk_load_test.py
+++ bulk_load_test.py
@@ -1,5 +1,6 @@
 import os
 import time
+from collections.abc import Iterator
 from typing import Type
 from typedb.api.connection.driver import TypeDBDriver
 from typedb.api.connection.session import SessionType
@@ -16,63 +17,67 @@ class BulkLoadTest:
         batch_size: int,
         transaction_count: int,
         config: Config,
-        logger: Logger = Logger()
+        logger: Logger = None
     ):
         self.driver = driver
         self.batch_size = batch_size
         self.transaction_count = transaction_count
-        self.dataset_dir = config.dataset_dir
-        self.schema_file = config.schema_file
-        self.data_files = config.data_files
-        self.database = config.database
-        self.use_async = config.use_async
-        self.test_reattempt_wait = config.test_reattempt_wait
-        self.maximum_test_attempts = config.maximum_test_attempts
-        self.logger = logger
+        self.config = config
+
+        if logger is None:
+            self.logger = Logger()
+        else:
+            self.logger = logger
 
     @property
     def schema(self) -> str:
-        schema_path = f"{os.getcwd()}/{self.dataset_dir}/{self.schema_file}.tql"
+        schema_path = f"{os.getcwd()}/{self.config.dataset_dir}/{self.config.schema_file}.tql"
         return open(schema_path, "r").read()
 
     @property
     def bulk_loader_class(self) -> Type[BulkLoader | AsyncBulkLoader]:
-        if self.use_async:
+        if self.config.use_async:
             return AsyncBulkLoader
         else:
             return BulkLoader
 
-    def run(self) -> dict[str, int | float]:
+    def run(self) -> dict:
         attempt_count = 1
+        self.logger.info(f"Starting test.")
 
-        while attempt_count <= self.maximum_test_attempts:
+        while attempt_count <= self.config.maximum_test_attempts:
             try:
-                self.logger.info(f"Using async loader: {str(self.use_async).lower()}")
+                self.logger.info(f"Using async loader: {str(self.config.use_async).lower()}")
                 self.logger.info(f"Using batch size: {self.batch_size}")
                 self.logger.info(f"Using transaction count: {self.transaction_count}")
 
-                if self.use_async and self.batch_size > os.cpu_count():
+                if self.config.use_async and self.batch_size > os.cpu_count():
                     self.logger.warn(f"Transaction count exceeds CPU count.")
 
-                result: dict[str, int | float] = dict()
+                result = {
+                    "async_loader": self.config.use_async,
+                    "batch_size": self.batch_size,
+                    "transaction_count": self.transaction_count,
+                }
+
                 self.logger.info(f"  Creating database.")
 
-                if self.driver.databases.contains(self.database):
-                    self.driver.databases.get(self.database).delete()
+                if self.driver.databases.contains(self.config.database):
+                    self.driver.databases.get(self.config.database).delete()
 
-                self.driver.databases.create(self.database)
+                self.driver.databases.create(self.config.database)
 
-                with self.driver.session(self.database, SessionType.SCHEMA) as session:
+                with self.driver.session(self.config.database, SessionType.SCHEMA) as session:
                     with session.transaction(TransactionType.WRITE) as transaction:
                         self.logger.info(f"  Defining schema.")
                         transaction.query.define(self.schema)
                         transaction.commit()
 
-                with self.driver.session(self.database, SessionType.DATA) as session:
-                    for file in self.data_files:
+                with self.driver.session(self.config.database, SessionType.DATA) as session:
+                    for file in self.config.data_files:
                         self.logger.info(f"  Loading data from file: {file}.tql")
                         query_count = 0
-                        data_path = f"{os.getcwd()}/{self.dataset_dir}/{file}.tql"
+                        data_path = f"{os.getcwd()}/{self.config.dataset_dir}/{file}.tql"
                         start = time.time()
                         bulk_loader = self.bulk_loader_class(data_path, session, self.batch_size, self.transaction_count)
                         bulk_loader.load()
@@ -87,8 +92,31 @@ class BulkLoadTest:
             except TypeDBDriverException as exception:
                 self.logger.warn(f"Test failed due to TypeDB exception: {exception}")
                 attempt_count += 1
-                time.sleep(self.test_reattempt_wait)
+                time.sleep(self.config.test_reattempt_wait)
                 self.logger.info(f"Re-starting test. Attempt: {attempt_count}")
 
-        self.logger.error(f"Maximum test attempts reached: {self.maximum_test_attempts}")
-        raise RuntimeError(f"Maximum test attempts reached: {self.maximum_test_attempts}")
+        self.logger.error(f"Maximum test attempts reached: {self.config.maximum_test_attempts}")
+        raise RuntimeError(f"Maximum test attempts reached: {self.config.maximum_test_attempts}")
+
+
+class BulkLoadTestBatch:
+    def __init__(
+        self,
+        driver: TypeDBDriver,
+        config: Config,
+        logger: Logger = None
+    ):
+        self.driver = driver
+        self.config = config
+
+        if logger is None:
+            self.logger = Logger()
+        else:
+            self.logger = logger
+
+    def run(self) -> Iterator[dict]:
+        for batch_size in self.config.batch_sizes:
+            for transaction_count in self.config.transaction_counts:
+                bulk_load = BulkLoadTest(self.driver, batch_size, transaction_count, self.config)
+                result = bulk_load.run()
+                yield result
--- data_loader.py
+++ data_loader.py
@@ -2,36 +2,34 @@ import datetime
 import getpass
 import os
 from typedb.driver import TypeDBCredential, TypeDB
-from bulk_load_test import BulkLoadTest
+from bulk_load_test import BulkLoadTestBatch
 from utils import Config, Logger
 
 config = Config()
+timestamp = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 password = getpass.getpass()
 credential = TypeDBCredential(config.username, password, tls_enabled=True)
-timestamp = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
-os.makedirs(f"{os.getcwd()}/{config.logs_dir}", exist_ok=True)
 log_path = f"{os.getcwd()}/{config.logs_dir}/{timestamp}.txt"
-logger = Logger(log_path)
-os.makedirs(f"{os.getcwd()}/{config.results_dir}", exist_ok=True)
 output_path = f"{os.getcwd()}/{config.results_dir}/{timestamp}.csv"
-header = "batch_size,transaction_count"
+os.makedirs(f"{os.getcwd()}/{config.logs_dir}", exist_ok=True)
+os.makedirs(f"{os.getcwd()}/{config.results_dir}", exist_ok=True)
+logger = Logger(log_path)
+header = "async_loader,batch_size,transaction_count"
 
 for file in config.data_files:
     header += f",{file}_count,{file}_time"
 
-with open(output_path, "w") as output:
-    output.write(f"{header}\n")
+with TypeDB.cloud_driver(config.addresses, credential) as driver:
+    with open(output_path, "w") as output:
+        output.write(f"{header}\n")
+        test_batch = BulkLoadTestBatch(driver, config, logger)
 
-    with TypeDB.cloud_driver(config.addresses, credential) as driver:
-        for batch_size in config.batch_sizes:
-            for transaction_count in config.transaction_counts:
-                bulk_load = BulkLoadTest(driver, batch_size, transaction_count, config)
-                result = bulk_load.run()
-                entry = f"{batch_size},{transaction_count}"
+        for result in test_batch.run():
+            entry = f"""{str(result["async_loader"]).lower()},{result["batch_size"]},{result["transaction_count"]}"""
 
-                for file in config.data_files:
-                    count_key = f"{file}_count"
-                    time_key = f"{file}_time"
-                    entry += f",{result[count_key]},{result[time_key]}"
+            for file in config.data_files:
+                count_key = f"{file}_count"
+                time_key = f"{file}_time"
+                entry += f",{result[count_key]},{result[time_key]}"
 
-                output.write(f"{entry}\n")
+            output.write(f"{entry}\n")
